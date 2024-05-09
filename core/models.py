bc89e2014a441bcbf2ba3803aeca1f1aea86441e
neetchea <medouni.sarra@gmail.com>
2024-05-09 10:20:48 +0100

    login added

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ('admin', 'Admin'),
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
+        ('parent', 'Parent'),
+    )
+
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'core/home.html')
+
+@login_required
+def dashboard(request):
+    if request.user.user_type == 'teacher':
+        template_name = 'core/teacher_dashboard.html'
+    elif request.user.user_type == 'student':
+        template_name = 'core/student_dashboard.html'
+    elif request.user.user_type == 'parent':
+        template_name = 'core/parent_dashboard.html'
+    else:
+        return redirect('home')
+
+    return render(request, template_name)
