8dede6445476aaada8109e98031c2b118d508e73
neetchea <medouni.sarra@gmail.com>
2024-05-13 09:22:12 +0100

    login working

--- core/models.py
+++ core/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 class CustomUser(AbstractUser):
+    
     USER_TYPE_CHOICES = (
         ('admin', 'Admin'),
         ('teacher', 'Teacher'),
@@ -10,3 +12,14 @@ class CustomUser(AbstractUser):
     )
 
     user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
+#     def save(self, *args, **kwargs):
+#         #     Ensure the password is hashed before saving
+#             if self.password:
+#                 self.set_password(self.password)
+#             super(CustomUser, self).save(*args, **kwargs)
+
+
+    def save(self, *args, **kwargs):
+        if self.password:
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
